11d141b896e4900f2fa46fea757e49bf4cfcae95
Eric Sergio <ericdsergio87@icloud.com>
2022-04-16 01:33:17 -0700

    populate mongo

---
+++ orderSchema.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+    StoreID: {
+        type: Number,
+        required:true
+    },
+    SalesPersonID: {
+        type: Number,
+        required:true
+    },
+    CdID: {
+        type: Number,
+        required:true
+    },
+    PricePaid: {
+        type: String,
+        required:true
+    },
+    Date: {
+        type: String,
+        required:true
+    }
+});
+
+module.exports = mongoose.model("Orders", OrderSchema);
+
+/*
+"StoreID":98046
+"SalesPersonID":22
+"CdID":621453
+"PricePaid":6
+"Date":1650066838710
+*/
--- routes/index.js
+++ routes/index.js
@@ -1,102 +1,42 @@
-let x = 2;
-var express = require('express');
+
+var express = require("express");
 var router = express.Router();
-var fs = require("fs");
+var mongoose = require("mongoose");
+const dbURI = "mongodb+srv://es31:twentYfiv3@cluster0.p2bvs.mongodb.net/Orders?retryWrites=true&w=majority";
+const OrderSchema = require("../orderSchema");
 
-// start by creating data so we don't have to type it in each time
-let ObjectOrderArray = [];
+mongoose.connect(dbURI).then(
+  () => {
+    console.log("DB Connection Successful");
+  },
+  err => {
+    console.log("Error Connecting due to : ", err)
+  }
+)
 
-// define a constructor to create movie objects
-let MovieObject = function (pTitle, pYear, pGenre, pMan, pWoman, pURL) {
-    this.ID = Math.random().toString(16).slice(5)  // tiny chance could get duplicates!
-    this.Title = pTitle;
-    this.Year = pYear;
-    this.Genre = pGenre;  // action  comedy  drama  horrow scifi  musical  western
-}
 
-// my file management code, embedded in an object
-fileManager  = {
 
-  // this will read a file and put the data in our movie array
-  // NOTE: both read and write files are synchonous, we really can't do anything
-  // useful until they are done.  If they were async, we would have to use call backs.
-  // functions really should take in the name of a file to be more generally useful
-  read: function() {
-    // has extra code to add 4 movies if and only if the file is empty
-    const stat = fs.statSync('objectOrder.json');
-    if (stat.size !== 0) {                           
-    var rawdata = fs.readFileSync('objectOrder.json'); // read disk file
-    ObjectOrderArray = JSON.parse(rawdata);  // turn the file data into JSON format and overwrite our array
+
+router.post('/AddOrder', function(req, res) {
+  let oneOrder = new OrderSchema(req.body);  
+  console.log(req.body);
+  oneOrder.save((err, todo) => {
+    if (err) {
+      res.status(500).send(err);
     }
     else {
-      // make up 3 for testing
-      fileManager.write();
-    }
-  },
-  
-  write: function() {
-    let data = JSON.stringify(ObjectOrderArray);    // take our object data and make it writeable
-    fs.writeFileSync('objectOrder.json', data);  // write it
-  },
-}
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile('index.html');
-});
-
-/* GET all Movie data */
-router.get('/getAllMovies', function(req, res) {
-  fileManager.read();
-  res.status(200).json(ObjectOrderArray);
-});
-
-
-/* Add one new Order */
-router.post('/AddOrder', function(req, res) {
-  const newOrder = req.body;  // get the object from the req object sent from browser
-  console.log(newOrder);
-  ObjectOrderArray.push(newOrder);  // add it to our "DB"  (array)
-  fileManager.write();
-  // prepare a reply to the browser
-  var response = {
-    status  : 200,
-    success : 'Added Successfully'
-  }
-  res.end(JSON.stringify(response)); // send reply
-});
-
-// delete movie
-
-router.delete('/DeleteMovie/:ID', (req, res) => {
-  const ID = req.params.ID;
-  let found = false;
-  console.log(ID);    
-
-  for(var i = 0; i < ObjectOrderArray.length; i++) // find the match
-  {
-      if(ObjectOrderArray[i].ID === ID){
-        ObjectOrderArray.splice(i,1);  // remove object from array
-          found = true;
-          fileManager.write();
-          break;
-      }
-  }
-
-  if (!found) {
-    console.log("not found");
-    return res.status(500).json({
-      status: "error"
-    });
-  } else {
     var response = {
       status  : 200,
-      success : 'Movie ' + ID + ' deleted!'
+      success : 'Added Successfully'
     }
     res.end(JSON.stringify(response)); // send reply
-  }
+
+    }
+  });
 });
 
+
+const port = process.env.PORT || 3000;
 
 module.exports = router;
